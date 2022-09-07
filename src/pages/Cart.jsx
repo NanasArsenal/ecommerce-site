1a13efea4ebf2e5b20d14eb55f199fecf2b87556
Nana Kweku <nanakwekudev@gmail.com>
2022-09-07 19:35:04 +0000

    cart change made

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,7 @@
 import React,{useContext} from 'react'
 import { CartContext } from '../Context/CartContext';
+import Card from '../components/shared/Card';
+
 
 const Cart = () => {
     
@@ -7,12 +9,17 @@ const Cart = () => {
 
 
   return (
-    <div>
+    <div className='w-full py-10 px-10   bg-slate-100  grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5 grid-rows-3 '>
       {Cartcont.cartitems.map((cartitem) =>{
 
-       return <div>
-          {cartitem.item}
-        </div>
+       return  <Card key={cartitem.id}>
+       <div>
+           <img src={cartitem.image} alt="" className='h-[450px] w-full p-2 '/>
+       </div>
+       <div className='p-2 px-3 flex justify-between'>
+           {cartitem.item}
+       </div>
+   </Card>
       })}
     </div>
   )
